adfc9901f9aa4f85411fdb723d886f5cf0d8e24f
zoe <ZoeOosting@outlook.com>
2022-10-03 13:01:24 +0200

    switch to clap derive because it's cooler

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,37 @@
-use clap::{ArgMatches, Command};
+use clap::{ArgMatches, Args, Command, Parser};
+use serde::{Deserialize, Serialize};
 
-/// parses command line arguments
-pub fn cli_get_matches() -> ArgMatches {
-    Command::new("rssc-intract")
-        .about("an algorithm replacement")
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-        .subcommand(Command::new("start"))
-        .get_matches()
+// defines all different subcommands
+#[derive(Parser)]
+pub enum Subcommand {
+    Start,
+}
+
+/// this defines all the cli commands
+#[derive(Parser)]
+#[command(version, about)]
+pub struct Cli {
+    /// this is the config file you want to use,
+    /// specifying this overwrites the default path
+    #[arg(short, long)]
+    pub config: Option<String>,
+
+    #[command(subcommand)]
+    pub subcommand: Subcommand,
+}
+
+#[derive(Default, Serialize, Deserialize, Debug)]
+pub struct ServerConfig {
+    pub frontend: FrontendConfig,
+}
+
+#[derive(Default, Serialize, Deserialize, Debug)]
+pub struct FrontendConfig {
+    /// relative path to the web frontend location,
+    /// this is where your index.html should be
+    pub location: String,
+}
+
+pub fn get_config_file() -> ServerConfig {
+    confy::load("rssc-intract", "config").unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
+use clap::{ArgMatches, Parser};
 use rocket::fs::FileServer;
 
 #[macro_use]
 extern crate rocket;
+
+#[macro_use]
+extern crate lazy_static;
+
 /// this module defines the client api
 mod api;
+
 /// this module defines config and command line arguments
 mod config;
+
 /// this is where the actual logic lives
 pub mod logic;
 
+lazy_static! {
+    static ref CLI: config::Cli = config::Cli::parse();
+    static ref CONF: config::ServerConfig = config::get_config_file();
+}
+
 #[launch]
 fn launch() -> _ {
-    match config::cli_get_matches().subcommand() {
-        Some(("start", _)) => rocket::build()
-            .mount("/", FileServer::from("static/"))
+    match CLI.subcommand {
+        config::Subcommand::Start => rocket::build()
+            .mount("/", FileServer::from(&CONF.frontend.location))
             .mount("/api", api::routes()),
-        _ => unreachable!(),
     }
 }
