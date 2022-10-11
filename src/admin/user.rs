d1500d288d322b5ce05e59915cbcb6786049d556
zoe <ZoeOosting@outlook.com>
2022-10-11 20:08:28 +0200

    finish up user menu

--- src/admin/user.rs
+++ src/admin/user.rs
@@ -1,6 +1,6 @@
 use crate::database as db;
 use crate::logic::auth;
-use dialoguer::{Input, Password};
+use dialoguer::{Confirm, FuzzySelect, Input, Password};
 
 use crate::admin::structs::*;
 
@@ -8,16 +8,24 @@ pub fn usermenu() -> MenuPage {
     MenuPage {
         items: vec![
             MenuItem {
+                name: "list users".to_string(),
+                function: || userpicker(Action::List),
+            },
+            MenuItem {
                 name: "add user".to_string(),
                 function: add_user,
             },
             MenuItem {
-                name: "delete users".to_string(),
-                function: || deletemenu()?.open(),
+                name: "change password".to_string(),
+                function: || userpicker(Action::ChangePw),
             },
             MenuItem {
-                name: "quit".to_string(),
-                function: || Ok(()),
+                name: "delete user".to_string(),
+                function: || userpicker(Action::Delete),
+            },
+            MenuItem {
+                name: "back".to_string(),
+                function: || crate::admin::main_menu().open(),
             },
         ],
     }
@@ -37,23 +45,52 @@ pub fn add_user() -> Result<(), Box<dyn std::error::Error>> {
     auth::add_user(username, password)
 }
 
+enum Action {
+    Delete,
+    List,
+    ChangePw,
+}
+
 /**
  * shows a menu page with all users who have accounts
  */
-fn deletemenu() -> Result<MenuPage, Box<dyn std::error::Error>> {
-    let users = db::user::get_all()?
+fn userpicker(action: Action) -> Result<(), Box<dyn std::error::Error>> {
+    let users = db::user::get_local()?;
+    let items = users
         .iter()
-        .map(|u| MenuItem {
-            function: || Ok(()),
-            name: u.username.clone(),
-        })
-        .collect::<Vec<MenuItem>>();
-    Ok(MenuPage { items: vec![] })
+        .map(|u| u.username.clone())
+        .collect::<Vec<String>>();
+    let index = FuzzySelect::new().items(&items).interact()?;
+    match action {
+        Action::Delete => {
+            return confirm_delete(users[index].username.clone());
+        }
+        Action::List => {
+            return Ok(());
+        }
+        Action::ChangePw => {
+            return change_password(users[index].username.clone());
+        }
+    }
 }
 
 /**
  * confirm if a user should be deleted
  */
 fn confirm_delete(username: String) -> Result<(), Box<dyn std::error::Error>> {
+    if !Confirm::new()
+        .with_prompt(format!("really delete {}", &username))
+        .interact()?
+    {
+        return Ok(());
+    }
+    db::user::delete(username)?;
     Ok(())
+}
+
+fn change_password(username: String) -> Result<(), Box<dyn std::error::Error>> {
+    let pw = Password::new()
+        .with_prompt(format!("new password for: {}", &username))
+        .interact()?;
+    auth::change_password(username, pw)
 }
