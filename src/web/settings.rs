a8d3d4abb693c8353e4788c8e0c45eca7b9bc085
zoe <ZoeOosting@outlook.com>
2022-10-17 15:01:45 +0200

    frontend for changing password

--- src/web/settings.rs
+++ src/web/settings.rs
@@ -1,6 +1,10 @@
-use crate::logic::{auth::Session, settings::ProfileSettings};
+use crate::logic::{
+    auth::Session,
+    settings::{PasswordSettings, ProfileSettings},
+};
 use rocket::{form::Form, response::Redirect, Route};
 use rocket_dyn_templates::{context, Template};
+use std::error::Error;
 
 pub fn routes() -> Vec<Route> {
     routes![
@@ -13,12 +17,7 @@ pub fn routes() -> Vec<Route> {
 
 #[get("/settings")]
 fn settings(session: Session) -> Template {
-    Template::render(
-        "settings",
-        context! {
-            displayname: session.user.display_name,
-        },
-    )
+    show(session, Ok(()))
 }
 
 #[get("/settings", rank = 2)]
@@ -27,12 +26,33 @@ fn redirect_settings() -> Redirect {
 }
 
 #[post("/settings/profile", data = "<settings>")]
-fn change_profile_settings(session: Session, settings: Form<ProfileSettings<'_>>) -> Redirect {
-    let _save = settings.save(&session.user.username);
-    Redirect::to("/settings")
+fn change_profile_settings(session: Session, settings: Form<ProfileSettings<'_>>) -> Template {
+    let save = settings.save(&session.user.username);
+    show(session, save)
 }
 
-#[post("/settings/password")]
-fn change_password_settings(_session: Session) -> Redirect {
-    Redirect::to("/settings")
+#[post("/settings/password", data = "<settings>")]
+fn change_password_settings(session: Session, settings: Form<PasswordSettings<'_>>) -> Template {
+    let save = settings.change_password(&session.user.username);
+    show(session, save)
+}
+
+fn show(session: Session, r: Result<(), Box<dyn Error>>) -> Template {
+    let template = render_template(session);
+    match r {
+        Ok(_) => template,
+        Err(e) => {
+            error!("{}", e);
+            template
+        }
+    }
+}
+
+fn render_template(session: Session) -> Template {
+    Template::render(
+        "usersettings",
+        context! {
+            displayname: session.user.display_name,
+        },
+    )
 }
